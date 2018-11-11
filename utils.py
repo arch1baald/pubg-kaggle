a43077ed1f32bc10fd9ba3c1dc445529f506ecba
Dmitry Savostyanov <ds@gosu.ai>
2018-11-11 21:14:36 +0200

    fix GBM2.ipynb         TODO:                 - adjustment function                 - train: CV assess function with respect to LightGBM                 - tuning: Tune GBM with Bayesian Opt                 - feature selection: update feature importances, try another threshold                 - profiling: time and memory for every steps in pipeline

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import pickle
 from datetime import datetime
 
 import pandas as pd
+import numpy as np
 from sklearn.model_selection import KFold
 
 
@@ -18,7 +19,7 @@ def camelcase_to_underscore(string):
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).lower()
 
 
-def load_data(file_name, directory='../input/', nrows=None):
+def load_data(file_name, directory='../input/', sample_size=None):
     """
     Load data from .csv file.
     Transform columns names from CamelCase to _underscore notation.
@@ -35,8 +36,51 @@ def load_data(file_name, directory='../input/', nrows=None):
         full_file_name = 'sample_submission_V2.csv'
     else:
         full_file_name = file_name
-    df = pd.read_csv(os.path.join(directory, full_file_name), nrows=nrows)
+    print('Loading ...')
+    df = pd.read_csv(os.path.join(directory, full_file_name), nrows=sample_size)
+    print('Compressing ...')
+    df = reduce_mem_usage(df)
     df.columns = [camelcase_to_underscore(col) for col in df.columns]
+    return df
+
+
+def reduce_mem_usage(df):
+    """
+    Iterate through all the columns of a dataframe and modify the data type to reduce memory usage
+
+    Source: https://www.kaggle.com/gemartin/load-data-reduce-memory-usage
+    :param df: DataFrame
+    :return:
+    """
+    start_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+
+    for col in df.columns:
+        col_type = df[col].dtype
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+
+    end_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    print('Decreased by {:.1f}%'.format(
+        100 * (start_mem - end_mem) / start_mem))
     return df
 
 
